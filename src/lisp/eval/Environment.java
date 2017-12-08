f9b113016f578d8a6e43ce83bdeef6ff66192cd0
sam0830 <samcreate0830@gmail.com>
2017-12-08 09:20:22 +0900

    環境の追加

---
+++ src/lisp/eval/Environment.java
@@ -0,0 +1,43 @@
+package lisp.eval;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 環境
+ * @author sam0830
+ *
+ */
+public class Environment {
+	private final Map<Symbol, SExpression> frame = new HashMap<>();
+	
+	private final Environment next;
+	
+	public Environment(Environment next) {
+		this.next = next;
+	}
+	
+	private static Environment find(Environment environment, Symbol key) {
+		for(Environment e = environment; e != null; e=e.next) {
+			if(e.frame.containsKey(key)) {
+				return e;
+			}
+		}
+		throw new RuntimeException("Variable " + key + " not found");
+	}
+	
+	public SExpression get(Symbol key) {
+		return find(this, key).frame.get(key);
+	}
+	
+	public SExpression set(Symbol key, SExpression value) {
+		find(this, key).frame.put(key, value);
+		return value;
+	}
+	
+	public SExpression define(Symbol key, SExpression value) {
+		frame.put(key, value);
+		return value;
+	}
+	
+}
