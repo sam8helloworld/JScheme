b888fddd7c4f034a6fba7b67be1418b875710873
sam0830 <samcreate0830@gmail.com>
2018-01-01 03:34:24 +0900

    Carにエラー条件追加

--- src/lisp/eval/Car.java
+++ src/lisp/eval/Car.java
@@ -10,13 +10,22 @@ public class Car implements Subroutine {
 	public SExpression apply(SExpression sexp, Environment environment) {
 		// car
 		// ConsCellが渡されなければエラー
+		// ConsCellのサイズが1でなければエラー
+		// 引数がConsCellでなければエラー
 		// ConsCellが渡されてていればcarを返す
-		if(sexp instanceof ConsCell) {
-			return ((ConsCell)((ConsCell) sexp).getCar()).getCar();
+		if(!(sexp instanceof ConsCell)) {
+			throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
 		}
-		throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
+		if(((ConsCell)sexp).size() != 1) {
+			throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
+		}
+		SExpression arg = ((ConsCell)sexp).get(0); // 第一引数
+		if(!(arg instanceof ConsCell)) {
+			throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
+		}
+		return ((ConsCell)arg).getCar();
+		
 	}
-	
 	
 	public static Car getInstance() {
 		return car;
