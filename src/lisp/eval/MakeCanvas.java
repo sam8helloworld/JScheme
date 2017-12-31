5a08d786a6c57c44c9ddaf39c010c4a0161ef24c
sam0830 <samcreate0830@gmail.com>
2018-01-01 05:38:11 +0900

    MakeCanvasの引数のとり方を追加

--- src/lisp/eval/MakeCanvas.java
+++ src/lisp/eval/MakeCanvas.java
@@ -6,13 +6,32 @@ package lisp.eval;
  *
  */
 public class MakeCanvas implements Subroutine {
+	private static final Int STANDARD_POSITION = Int.valueOf(200);
+	private static final Int STANDARD_SIZE = Int.valueOf(400);
 	private static final MakeCanvas makeCanvas = new MakeCanvas();
 	
 	public SExpression apply(SExpression sexp, Environment environment) {
-		if(!(sexp instanceof EmptyList)) {
-			// 引数無し以外はエラー
+		// 引数無しの時は自動でサイズ決定
+		if(sexp instanceof EmptyList) {
+			return Canvas.getInstance(STANDARD_POSITION, STANDARD_POSITION, STANDARD_SIZE, STANDARD_SIZE);
 		}
-		return Canvas.getInstance(Int.valueOf(200), Int.valueOf(200), Int.valueOf(200), Int.valueOf(200));
+		// 引数ありの時はNumber型で4つが必須
+		if(!(sexp instanceof ConsCell)) {
+			// エラー
+		}
+		int size = ((ConsCell)sexp).size();
+		if(size != 4) {
+			// エラー
+		}
+		Number[] value = new Number[4];
+		for(int i=0;i<size;i++) {
+			SExpression tmp = ((ConsCell)sexp).get(i);
+			if(!(tmp instanceof Number)) {
+				// エラー
+			}
+			value[i] = (Number)tmp;
+		}
+		return Canvas.getInstance(value[0], value[1], value[2], value[3]);
 	}
 	
 	public static MakeCanvas getInstance() {
