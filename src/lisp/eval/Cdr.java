3b30088e72dfe8805747e004593c1212b2f039e8
sam0830 <samcreate0830@gmail.com>
2018-01-05 04:00:57 +0900

    Cdrにエラー処理を追加

--- src/lisp/eval/Cdr.java
+++ src/lisp/eval/Cdr.java
@@ -1,5 +1,8 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
  * Cdr
  * @author sam0830
@@ -7,26 +10,29 @@ package lisp.eval;
  */
 public class Cdr implements Subroutine {
 	private static final Cdr cdr = new Cdr();
-	public SExpression apply(SExpression sexp, Environment environment) {
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		// cdr
 		// ConsCellが渡されなければエラー
 		// ConsCellのサイズが1でなければエラー
 		// 引数がConsCellでなければエラー
 		// ConsCellが渡されてていればcdrを返す
 		if(!(sexp instanceof ConsCell)) {
-			throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
+			throw new ArgumentException("pair required, but got "+sexp);
 		}
-		if(((ConsCell)sexp).size() != 1) {
-			throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
+		int size = ((ConsCell)sexp).size();
+		if(size != 1) {
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 1, got "+size+")");
 		}
 		SExpression arg = ((ConsCell)sexp).get(0); // 第一引数
 		if(!(arg instanceof ConsCell)) {
-			throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
+			throw new ArgumentException("pair required, but got "+arg);
 		}
 		return ((ConsCell)arg).getCdr();
 	}
-	
-	
+	@Override
+	public String toString() {
+		return "<subr cdr>";
+	}
 	public static Cdr getInstance() {
 		return cdr;
 	}
