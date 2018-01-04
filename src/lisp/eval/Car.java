887fad89ea0fcf646a7b952d9303d01ff1130ed8
sam0830 <samcreate0830@gmail.com>
2018-01-05 03:58:00 +0900

    Carにエラー処理を追加

--- src/lisp/eval/Car.java
+++ src/lisp/eval/Car.java
@@ -1,5 +1,8 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
  * Car
  * @author sam0830
@@ -7,26 +10,30 @@ package lisp.eval;
  */
 public class Car implements Subroutine {
 	private static final Car car = new Car();
-	public SExpression apply(SExpression sexp, Environment environment) {
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		// car
 		// ConsCellが渡されなければエラー
 		// ConsCellのサイズが1でなければエラー
 		// 引数がConsCellでなければエラー
 		// ConsCellが渡されてていればcarを返す
 		if(!(sexp instanceof ConsCell)) {
-			throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
+			throw new ArgumentException("pair required, but got "+sexp);
 		}
-		if(((ConsCell)sexp).size() != 1) {
-			throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
+		int size = ((ConsCell)sexp).size();
+		if(size != 1) {
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 1, got "+size+")");
 		}
 		SExpression arg = ((ConsCell)sexp).get(0); // 第一引数
 		if(!(arg instanceof ConsCell)) {
-			throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
+			throw new ArgumentException("pair required, but got "+arg);
 		}
 		return ((ConsCell)arg).getCar();
 		
 	}
-	
+	@Override
+	public String toString() {
+		return "<subr car>";
+	}
 	public static Car getInstance() {
 		return car;
 	}
