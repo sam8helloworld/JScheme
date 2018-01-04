58eac034afb464f31a878254bc9234140aff6b84
sam0830 <samcreate0830@gmail.com>
2018-01-05 03:47:09 +0900

    AskNullにエラー処理を追加

--- src/lisp/eval/AskNull.java
+++ src/lisp/eval/AskNull.java
@@ -1,5 +1,8 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
  * null?
  * @author sam0830
@@ -12,14 +15,15 @@ public class AskNull implements Subroutine {
 		return askNull;
 	}
 	@Override
-	public SExpression apply(SExpression sexp, Environment environment) {
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		// 引数が空リストかどうか
 		if(!(sexp instanceof ConsCell)) {
-			throw new RuntimeException("引数がない");
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 1, got 0)");
 		}
 		// 引数の個数は1個
-		if(((ConsCell)sexp).size() != 1) {
-			throw new RuntimeException("引数の個数が1個以上ある");
+		int size = ((ConsCell)sexp).size();
+		if(size != 1) {
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 1, got "+size+")");
 		}
 		return (((ConsCell)sexp).get(0) instanceof EmptyList)?Bool.valueOf(true):Bool.valueOf(false);
 	}
