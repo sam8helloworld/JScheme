df85f25a1c11c458e940878c1a95b63a69701f9c
sam0830 <samcreate0830@gmail.com>
2018-01-05 04:16:19 +0900

    Consにエラー処理を追加

--- src/lisp/eval/Cons.java
+++ src/lisp/eval/Cons.java
@@ -1,5 +1,8 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
  * Cons
  * @author sam0830
@@ -8,19 +11,23 @@ package lisp.eval;
 public class Cons implements Subroutine {
 	private static final Cons cons = new Cons();
 	
-	public SExpression apply(SExpression sexp, Environment environment) {
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		// 引数2個以外ではエラー
 		if(!(sexp instanceof ConsCell)) {
-			// エラー
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 2, got 0)");
 		}
-		if(!(((ConsCell)sexp).size() == 2)) {
-			// エラー
+		int size = ((ConsCell)sexp).size();
+		if(size != 2) {
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 2, got "+size+")");
 		}
 		SExpression car = ((ConsCell)sexp).getCar();
 		SExpression cdr = ((ConsCell)((ConsCell)sexp).getCdr()).getCar();
 		return ConsCell.getInstance(car, cdr);
 	}
-	
+	@Override
+	public String toString() {
+		return "#<subr cons>";
+	}
 	public static Cons getInstance() {
 		return cons;
 	}
