7d8cc9de886d017a4da3a06e5da4f6b2e47e2915
sam0830 <samcreate0830@gmail.com>
2018-01-05 04:13:24 +0900

    Closureにエラー処理を追加

--- src/lisp/eval/Closure.java
+++ src/lisp/eval/Closure.java
@@ -1,5 +1,6 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
 import lisp.exception.LispException;
 
 /**
@@ -20,17 +21,19 @@ public class Closure implements SExpression {
 		// 仮引数がシンボルの時
 		if(params instanceof EmptyList) {
 			if(!(sexp instanceof EmptyList)) {
-				throw new RuntimeException("引数の個数が違う");
+				throw new ArgumentException("wrong number of arguments for "+this+" (required 0, got 1)");
 			}
 		}
 		if(params instanceof ConsCell) {
-			if(((ConsCell)params).size() != ((ConsCell)sexp).size()) {
-				throw new RuntimeException("引数の個数が違う");
+			int requiredSize = ((ConsCell)params).size();
+			int actualSize = ((ConsCell)sexp).size();
+			if(requiredSize != actualSize) {
+				throw new ArgumentException("wrong number of arguments for "+this+" (required "+requiredSize+", got "+actualSize+")");
 			}
-			for(int i=0;i<((ConsCell)params).size();i++) {
+			for(int i=0;i<requiredSize;i++) {
 				SExpression symbol = ((ConsCell)params).get(i);
 				if(!(symbol instanceof Symbol)) {
-					throw new RuntimeException("引数の型がSymbolでない");
+					throw new ArgumentException("symbol required: "+symbol);
 				}
 				env.define((Symbol)symbol, ((ConsCell)sexp).get(i));
 			}
@@ -48,7 +51,10 @@ public class Closure implements SExpression {
 		}
 		return Evaluator.eval(this.body, env);
 	}
-	
+	@Override
+	public String toString() {
+		return "#<closure>";
+	}
 	private Closure(SExpression body, SExpression params) {
 		this.body = body;
 		this.params = params;
