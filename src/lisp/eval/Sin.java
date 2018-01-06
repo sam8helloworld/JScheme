3762e3f2e3400c1632131bbabd9e90a09a6b87ff
sam0830 <samcreate0830@gmail.com>
2018-01-07 02:21:49 +0900

    Sinのエラー処理追加

--- src/lisp/eval/Sin.java
+++ src/lisp/eval/Sin.java
@@ -1,5 +1,8 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
  * Sin
  * @author sam0830
@@ -8,16 +11,17 @@ package lisp.eval;
 public class Sin implements Subroutine {
 	private static final Sin sin = new Sin();
 	
-	public SExpression apply(SExpression sexp, Environment environment) {
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		if(!(sexp instanceof ConsCell)) {
-			// エラー
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 1, got 0)");
 		}
-		if(((ConsCell)sexp).size() != 1) {
-			// エラー
+		int size = ((ConsCell)sexp).size();
+		if(size != 1) {
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 1, got "+size+")");
 		}
 		SExpression car = ((ConsCell)sexp).getCar();
 		if(!(car instanceof Number)) {
-			// エラー
+			throw new ArgumentException("number required, but got "+car);
 		}
 		double number = (car instanceof Int)?((Int)car).getValue():((lisp.eval.Double)car).getValue();
 		return lisp.eval.Double.valueOf(Math.sin(Math.toRadians(number)));
@@ -26,5 +30,10 @@ public class Sin implements Subroutine {
 	
 	public static Sin getInstance() {
 		return sin;
+	}
+	
+	@Override
+	public String toString() {
+		return "#<subr sin>";
 	}
 }
