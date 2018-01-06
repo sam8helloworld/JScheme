2b4b3d9ee7639f765fbd9b1db3ebfa0527c22170
sam0830 <samcreate0830@gmail.com>
2018-01-07 03:54:33 +0900

    Boolにドキュメンテーションコメントを追加

--- src/lisp/eval/Bool.java
+++ src/lisp/eval/Bool.java
@@ -1,20 +1,29 @@
 package lisp.eval;
 
 /**
- * 真理値
+ * 真理値を表すS式
  * @author sam0830
- *
+ * @version 1.0
  */
-public class Bool implements SExpression{
+public class Bool implements SExpression {
 	private static final Bool TRUE = new Bool(true);
 	private static final Bool FALSE = new Bool(false);
 
 	private boolean value;
-
+	
+	/**
+	 * Boolのメンバ変数valueの真理値を返す
+	 * @return Boolのメンバ変数value
+	 */
 	public boolean isValid() {
 		return value;
 	}
 	
+	/**
+	 * Boolのインスタンスを返す
+	 * @param value 設定する真理値
+	 * @return S式Bool
+	 */
 	public static Bool valueOf(boolean value) {
 		if (value == true) {
 			return TRUE;
@@ -22,6 +31,10 @@ public class Bool implements SExpression{
 		return FALSE;
 	}
 	
+	/**
+	 * 真理値のsetter
+	 * @param value 設定する真理値
+	 */
 	private Bool(boolean value) {
 		this.value = value;
 	}
