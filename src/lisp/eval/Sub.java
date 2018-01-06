0780311a124dc4ea38d04acd088c84ac01515723
sam0830 <samcreate0830@gmail.com>
2018-01-07 02:24:57 +0900

    Subのエラー処理追加

--- src/lisp/eval/Sub.java
+++ src/lisp/eval/Sub.java
@@ -1,17 +1,20 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
- * Add (加法)
+ * Sub (減法)
  * @author sam0830
  *
  */
 public class Sub implements Subroutine{
 	private static final Sub sub = new Sub();
 	
-	public SExpression apply(SExpression sexp, Environment environment) {
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		// 引数なし
 		if(sexp instanceof EmptyList) {
-			// エラー
+			throw new ArgumentException("procedure requires at least one argument: (-)");
 		}
 		
 		SExpression car = ((ConsCell)sexp).getCar();
@@ -20,7 +23,7 @@ public class Sub implements Subroutine{
 		if(((ConsCell)sexp).size() == 1) {
 			// 引数が数値でない
 			if(!(car instanceof Number)) {
-				// エラー
+				throw new ArgumentException("operation - is not defined on object "+car);
 			}
 			// 引数が数値
 			return ((Number)car).multiply(Int.valueOf(-1));
@@ -28,14 +31,14 @@ public class Sub implements Subroutine{
 		
 		// 引数が2つ以上
 		if(!(car instanceof Number)) {
-			// エラー
+			throw new ArgumentException("operation - is not defined between "+car+" and "+((ConsCell)sexp).get(1));
 		}
 		Number number = (Number)car;
 		SExpression tmp = ((ConsCell)sexp).getCdr();
 		while(tmp instanceof ConsCell) {
 			SExpression arg = ((ConsCell)tmp).getCar();
 			if(!(arg instanceof Number)) {
-				// エラー
+				throw new ArgumentException("operation - is not defined between "+number+" and "+arg);
 			}
 			// 引数が数値
 			number = number.sub((Number)arg);
@@ -46,5 +49,10 @@ public class Sub implements Subroutine{
 	
 	public static Sub getInstance() {
 		return sub;
+	}
+	
+	@Override
+	public String toString() {
+		return "#<subr ->";
 	}
 }
