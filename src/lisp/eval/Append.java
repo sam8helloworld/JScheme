0ebac102151388f54ba812fe30138f2fd905a4ab
sam0830 <samcreate0830@gmail.com>
2018-01-07 03:38:41 +0900

    Appendにドキュメンテーションコメントを追加

--- src/lisp/eval/Append.java
+++ src/lisp/eval/Append.java
@@ -1,27 +1,45 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
- * Append
+ * リストとリストを結合したリストを作る
  * @author sam0830
- *
+ * @version 1.0
  */
 public class Append implements Subroutine {
 	private static final Append append = new Append();
 	
-	private SExpression append(SExpression list1, SExpression list2) {
+	/**
+	 * Appendのインスタンスを返す
+	 * @return 組み込み手続きAppend 
+	 */
+	public static Append getInstance() {
+		return append;
+	}
+	
+	/**
+	 * 
+	 * @param list1 結合されるリスト
+	 * @param list2 結合するリスト
+	 * @return 結合後のリスト
+	 * @throws LispException list1がリストでない時
+	 */
+	private SExpression append(SExpression list1, SExpression list2) throws LispException {
 		if(list1 instanceof EmptyList) {
 			return list2;
 		}
 		if(!(list1 instanceof ConsCell)) {
-			// エラー
-			throw new RuntimeException("エラー");
+			throw new ArgumentException("list required, but got "+list1);
 		}
 		return ConsCell.getInstance(((ConsCell)list1).getCar(),
 				                    append(((ConsCell)list1).getCdr(), list2)
 				                    );
 	}
 	
-	public SExpression apply(SExpression sexp, Environment environment) {
+	@Override
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		// appendの引数が空リスト 
 		if(!(sexp instanceof ConsCell)) {
 			return sexp;
@@ -36,7 +54,8 @@ public class Append implements Subroutine {
 		return appendedList;
 	}
 	
-	public static Append getInstance() {
-		return append;
+	@Override
+	public String toString() {
+		return "#<subr append>";
 	}
 }
