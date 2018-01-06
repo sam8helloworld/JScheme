024d6ada0eb3ba432bf946e272bd6d6bcc86ecee
sam0830 <samcreate0830@gmail.com>
2018-01-07 03:22:26 +0900

    Subroutineにドキュメンテーションコメントを追加

--- src/lisp/eval/Subroutine.java
+++ src/lisp/eval/Subroutine.java
@@ -3,10 +3,17 @@ package lisp.eval;
 import lisp.exception.LispException;
 
 /**
- * Sub routine (組み込み手続き)
+ * 組み込み手続きのインタフェース
  * @author sam0830
- *
+ * @version 1.0
  */
 interface Subroutine extends SExpression {
+	/**
+	 * 組み込み手続きを実行する
+	 * @param sexp 組み込み手続きの引数
+	 * @param environment 実行時の環境
+	 * @return 手続きの結果
+	 * @throws LispException 引数が手続きの望む型でない時
+	 */
 	public SExpression apply(SExpression sexp, Environment environment) throws LispException;
 }
