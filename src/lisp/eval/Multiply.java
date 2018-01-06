5402cffe0e492f17ca4226df75b7195cd3c7a5a2
sam0830 <samcreate0830@gmail.com>
2018-01-07 02:12:01 +0900

    Multiplyのエラー処理追加

--- src/lisp/eval/Multiply.java
+++ src/lisp/eval/Multiply.java
@@ -1,5 +1,7 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+
 /**
  * Multiply (乗法)
  * @author sam0830
@@ -8,13 +10,13 @@ package lisp.eval;
 public class Multiply implements Subroutine {
 	private static final Multiply multiply = new Multiply();
 	
-	public SExpression apply(SExpression sexp, Environment environment) {
+	public SExpression apply(SExpression sexp, Environment environment) throws ArgumentException {
 		Number number = Int.valueOf(1);
 		SExpression tmp = sexp;
 		while(tmp instanceof ConsCell) {
 			SExpression car = ((ConsCell)tmp).getCar();
 			if(!(car instanceof Number)) {
-				// エラー
+				throw new ArgumentException("operation * is not defined on object "+car);
 			}
 			// 引数が数値
 			number = number.multiply((Number)car);
@@ -25,5 +27,10 @@ public class Multiply implements Subroutine {
 	
 	public static Multiply getInstance() {
 		return multiply;
+	}
+	
+	@Override
+	public String toString() {
+		return "#<subr *>";
 	}
 }
