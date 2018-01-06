e51aa7919e05b08cbc4f4abde0bfe7f0dc49636e
sam0830 <samcreate0830@gmail.com>
2018-01-07 03:43:19 +0900

    AskNullにドキュメンテーションコメントを追加

--- src/lisp/eval/AskNull.java
+++ src/lisp/eval/AskNull.java
@@ -4,16 +4,22 @@ import lisp.exception.ArgumentException;
 import lisp.exception.LispException;
 
 /**
- * null?
+ * 引数が空リストかどうかを判別する
+ * コマンド名はnull?
  * @author sam0830
- *
+ * @version 1.0
  */
 public class AskNull implements Subroutine {
 	private static final AskNull askNull = new AskNull();
 	
+	/**
+	 * AskNullのインスタンスを返す
+	 * @return 組み込み手続きAskNull
+	 */
 	public static AskNull getInstance() {
 		return askNull;
 	}
+	
 	@Override
 	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		// 引数が空リストかどうか
@@ -27,6 +33,7 @@ public class AskNull implements Subroutine {
 		}
 		return (((ConsCell)sexp).get(0) instanceof EmptyList)?Bool.valueOf(true):Bool.valueOf(false);
 	}
+	
 	@Override
 	public String toString() {
 		return "#<subr null?>";
