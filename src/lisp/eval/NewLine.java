ef485efea1ba38b49b22f1ba63f6c04d2a0ac5bb
sam0830 <samcreate0830@gmail.com>
2018-01-07 02:13:51 +0900

    NewLineのエラー処理追加

--- src/lisp/eval/NewLine.java
+++ src/lisp/eval/NewLine.java
@@ -1,5 +1,8 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
  * NewLine(/改行)
  * @author sam0830
@@ -8,7 +11,7 @@ package lisp.eval;
 public class NewLine implements Subroutine {
 	private static final NewLine newLine = new NewLine();
 	
-	public SExpression apply(SExpression sexp, Environment environment) {
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		/**
 		 * 第一引数は出力ポート(省略の場合は標準出力)
 		 * 今回は出力ポートの指定は無し
@@ -18,10 +21,14 @@ public class NewLine implements Subroutine {
 			return Undef.getInstance();
 		}
 		// エラー
-		throw new RuntimeException("");
+		throw new ArgumentException("output port required, but got "+sexp);
 	}
 	
 	public static NewLine getInstance() {
 		return newLine;
+	}
+	@Override
+	public String toString() {
+		return "#<subr newline>";
 	}
 }
