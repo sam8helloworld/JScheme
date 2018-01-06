c8f5da49dd4d53a842d0f501a51bcb22b2837ca6
sam0830 <samcreate0830@gmail.com>
2018-01-06 23:22:49 +0900

    Devideのエラー処理追加

--- src/lisp/eval/Divide.java
+++ src/lisp/eval/Divide.java
@@ -1,5 +1,8 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
  * Multiply (乗法)
  * @author sam0830
@@ -8,10 +11,10 @@ package lisp.eval;
 public class Divide implements Subroutine {
  private static final Divide divide = new Divide();
 	
-	public SExpression apply(SExpression sexp, Environment environment) {
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		// 引数なし
 		if(sexp instanceof EmptyList) {
-			// エラー
+			throw new ArgumentException("procedure requires at least one argument: (/)");
 		}
 		
 		SExpression car = ((ConsCell)sexp).getCar();
@@ -20,7 +23,7 @@ public class Divide implements Subroutine {
 		if(((ConsCell)sexp).size() == 1) {
 			// 引数が数値でない
 			if(!(car instanceof Number)) {
-				// エラー
+				throw new ArgumentException("operation / is not defined on object "+car);
 			}
 			// 引数が数値
 			return Int.valueOf(1).divide((Number)car);
@@ -28,14 +31,14 @@ public class Divide implements Subroutine {
 		
 		// 引数が2個以上
 		if(!(car instanceof Number)) {
-			// エラー
+			throw new ArgumentException("operation / is not defined between "+car+" and "+((ConsCell)sexp).get(1));
 		}
 		Number number = (Number)car;
 		SExpression tmp = ((ConsCell)sexp).getCdr();
 		while(tmp instanceof ConsCell) {
 			SExpression arg = ((ConsCell)tmp).getCar();
 			if(!(arg instanceof Number)) {
-				// エラー
+				throw new ArgumentException("operation / is not defined between "+number+" and "+arg);
 			}
 			// 引数が数値
 			number = number.divide((Number)arg);
@@ -46,5 +49,10 @@ public class Divide implements Subroutine {
 	
 	public static Divide getInstance() {
 		return divide;
+	}
+	
+	@Override
+	public String toString() {
+		return "#<subr />";
 	}
 }
