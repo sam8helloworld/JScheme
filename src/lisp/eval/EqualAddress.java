b242067d6c3e07d86a6f82f67a1f1fee44fe930d
sam0830 <samcreate0830@gmail.com>
2018-01-06 23:56:41 +0900

    eq?のエラー処理を追加

--- src/lisp/eval/EqualAddress.java
+++ src/lisp/eval/EqualAddress.java
@@ -1,5 +1,8 @@
 package lisp.eval;
 
+import lisp.exception.ArgumentException;
+import lisp.exception.LispException;
+
 /**
  * EqualAddress
  * @author sam0830
@@ -10,14 +13,16 @@ public class EqualAddress implements Subroutine {
 	/**
 	 * 引数は2つのみ
 	 * 引数が2つでないときエラー
+	 * @throws LispException 
 	 */
-	public SExpression apply(SExpression sexp, Environment environment) { 
+	public SExpression apply(SExpression sexp, Environment environment) throws LispException { 
 		// 引数の個数が0個の時
 		if(sexp instanceof EmptyList) {
-			// エラー
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 2, got 0)");
 		}
-		if(((ConsCell)sexp).size() != 2) {
-			// エラー
+		int size = ((ConsCell)sexp).size();
+		if(size != 2) {
+			throw new ArgumentException("wrong number of arguments for "+this+" (required 2, got "+size+")");
 		}
 		// 引数の個数が2個の時
 		return ((ConsCell)sexp).get(0).equals(((ConsCell)sexp).get(1))?Bool.valueOf(true) : Bool.valueOf(false);
@@ -25,5 +30,10 @@ public class EqualAddress implements Subroutine {
 	
 	public static EqualAddress getInstance() {
 		return equalAddress;
+	}
+	
+	@Override
+	public String toString() {
+		return "#<subr eq?>";
 	}
 }
