b2f09adf4f3ca984e24817f0e7159a9b2419b977
sam0830 <samcreate0830@gmail.com>
2018-01-07 23:43:32 +0900

    Cosのドキュメンテーションコメントを追加

--- src/lisp/eval/Cos.java
+++ src/lisp/eval/Cos.java
@@ -4,13 +4,22 @@ import lisp.exception.ArgumentException;
 import lisp.exception.LispException;
 
 /**
- * Cos
+ * Cosを計算する組み込み手続き
  * @author sam0830
- *
+ * @version 1.0
  */
 public class Cos implements Subroutine {
 	private static final Cos cos = new Cos();
 	
+	/**
+	 * Cosのインスタンスを返す
+	 * @return 組み込み手続きCos
+	 */
+	public static Cos getInstance() {
+		return cos;
+	}
+	
+	@Override
 	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		if(!(sexp instanceof ConsCell)) {
 			throw new ArgumentException("wrong number of arguments for "+this+" (required 1, got 0)");
@@ -26,10 +35,6 @@ public class Cos implements Subroutine {
 		double number = (car instanceof Int)?((Int)car).getValue():((lisp.eval.Double)car).getValue();
 		return lisp.eval.Double.valueOf(Math.cos(Math.toRadians(number)));
 		
-	}
-	
-	public static Cos getInstance() {
-		return cos;
 	}
 	
 	@Override
