12ded4e0729bb2897630a1bdc18a3241447e4f65
sam0830 <samcreate0830@gmail.com>
2018-01-08 03:26:02 +0900

    undefのドキュメンテーションコメントを追加

--- src/lisp/eval/Undef.java
+++ src/lisp/eval/Undef.java
@@ -1,16 +1,22 @@
 package lisp.eval;
 
 /**
- * Undef(/未定義)
+ * Undef(/未定義)を表すS式
+ * displayやwriteでしか用いられない
  * @author sam0830
- *
+ * @version 1.0
  */
 public class Undef implements SExpression {
 	private static final Undef undef = new Undef();
 	
+	/**
+	 * Undefのインスタンスを返す
+	 * @return 組み込み手続きUndef
+	 */
 	public static Undef getInstance() {
 		return undef;
 	}
+	
 	@Override
 	public String toString() {
 		return "#<undef>";
