06ce37810387197cb9ef5e00102d90d66e3bbfdb
sam0830 <samcreate0830@gmail.com>
2018-01-08 03:17:02 +0900

    Quoteのドキュメンテーションコメントを修正

--- src/lisp/eval/Quote.java
+++ src/lisp/eval/Quote.java
@@ -1,21 +1,26 @@
 package lisp.eval;
 
 /**
- * Quote
+ * 特殊形式quote
+ * 引数を評価しない
  * @author sam0830
- *
+ * @version 1.0
  */
 public class Quote implements SpecialForm {
 	private static final Quote quote = new Quote();
+	
+	/**
+	 * Quoteのインスタンスを返す
+	 * @return 組み込み手続きQuote
+	 */
+	public static Quote getInstance() {
+		return quote;
+	}
+	
 	public SExpression apply(SExpression sexp, Environment environment) {
 		// quote 
 		// 評価せずにそのまま返す
 		return ((ConsCell)sexp).getCar();
-	}
-	
-	
-	public static Quote getInstance() {
-		return quote;
 	}
 	
 	@Override
