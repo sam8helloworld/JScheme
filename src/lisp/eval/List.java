3fd02196770f9f854dfcd62a4102ab27e830a63d
sam0830 <samcreate0830@gmail.com>
2018-01-08 03:08:48 +0900

    Listのドキュメンテーションコメントを追加

--- src/lisp/eval/List.java
+++ src/lisp/eval/List.java
@@ -1,20 +1,28 @@
 package lisp.eval;
 
 /**
- * List
+ * 組み込み手続きlist
  * @author sam0830
- *
+ * @version 1.0
  */
 public class List implements Subroutine {
 	private static final List list = new List();
-	public SExpression apply(SExpression sexp, Environment environment) {
-		// list
-		return sexp;
-		//throw new RuntimeException("*** ERROR: pair required, but got "+sexp);
-	}
 	
-	
+	/**
+	 * Listのインスタンスを返す
+	 * @return 組み込み手続きList
+	 */
 	public static List getInstance() {
 		return list;
+	}
+	
+	@Override
+	public SExpression apply(SExpression sexp, Environment environment) {
+		return sexp;
+	}
+	
+	@Override
+	public String toString() {
+		return "#<subr list>";
 	}
 }
