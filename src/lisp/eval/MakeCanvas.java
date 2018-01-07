d51b33a52c5535bb7cc8983f6a2790bb3240231b
sam0830 <samcreate0830@gmail.com>
2018-01-08 03:10:43 +0900

    MakeCanvasのドキュメンテーションコメントを追加

--- src/lisp/eval/MakeCanvas.java
+++ src/lisp/eval/MakeCanvas.java
@@ -4,15 +4,25 @@ import lisp.exception.ArgumentException;
 import lisp.exception.LispException;
 
 /**
- * MakeCanvas
+ * 組み込み手続きmake-canvas
+ * 描画用のWindowを作成する
  * @author sam0830
- *
+ * @version 1.0
  */
 public class MakeCanvas implements Subroutine {
 	private static final Int STANDARD_POSITION = Int.valueOf(200);
 	private static final Int STANDARD_SIZE = Int.valueOf(400);
 	private static final MakeCanvas makeCanvas = new MakeCanvas();
 	
+	/**
+	 * MakeCanvasのインスタンスを返す
+	 * @return 組み込み手続きMakeCanvas
+	 */
+	public static MakeCanvas getInstance() {
+		return makeCanvas;
+	}
+	
+	@Override
 	public SExpression apply(SExpression sexp, Environment environment) throws LispException {
 		// 引数無しの時は自動でサイズ決定
 		if(sexp instanceof EmptyList) {
@@ -32,10 +42,6 @@ public class MakeCanvas implements Subroutine {
 			value[i] = (Number)tmp;
 		}
 		return Canvas.getInstance(value[0], value[1], value[2], value[3]);
-	}
-	
-	public static MakeCanvas getInstance() {
-		return makeCanvas;
 	}
 	
 	@Override
