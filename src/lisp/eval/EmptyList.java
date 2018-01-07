cbf95d07a024b55dd66f6b039433d8fb90fccef0
sam0830 <samcreate0830@gmail.com>
2018-01-08 01:14:44 +0900

    EmptyListのドキュメンテーションコメントを追加

--- src/lisp/eval/EmptyList.java
+++ src/lisp/eval/EmptyList.java
@@ -1,17 +1,18 @@
 package lisp.eval;
 /**
- * 空リスト
+ * 空リストを表すS式
  * @author sam0830
- *
+ * @version 1.0
  */
-public class EmptyList implements SExpression{
+public class EmptyList implements SExpression {
 	private static final EmptyList emptyList = new EmptyList();
-
+	
+	/**
+	 * EmptyListのインスタンスを返す
+	 * @return 組み込み手続きEmptyList
+	 */
 	public static EmptyList getInstance() {
 		return emptyList;
-	}
-
-	private EmptyList() {
 	}
 
 	@Override
