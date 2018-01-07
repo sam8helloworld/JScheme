a08f0eeac3b4bdf8e4f9c9b75e2c56541529320e
sam0830 <samcreate0830@gmail.com>
2018-01-08 02:39:00 +0900

    Intのドキュメンテーションコメントを追加

--- src/lisp/eval/Int.java
+++ src/lisp/eval/Int.java
@@ -4,25 +4,39 @@ import lisp.exception.ArgumentException;
 import lisp.exception.LispException;
 
 /**
- * 整数値
+ * 整数を表すS式
  * @author sam0830
- *
+ * @version 1.0
  */
 public class Int implements SExpression, Number {
 	private Integer value;
 	
+	/**
+	 * int型の値を返す
+	 * @return int型の変数
+	 */
 	public Integer getValue() {
 		return value;
 	}
-
+	
+	/**
+	 * 整数型のS式のインスタンスを返す
+	 * @param value 整数のS式の中身のdouble
+	 * @return 整数型のS式のインスタンス
+	 */
 	public static Int valueOf(int value) {
 		return new Int(value);
 	}
 	
+	/**
+	 * コンストラクタ
+	 * @param value int型の整数
+	 */
 	private Int(Integer value) {
 		this.value = value;
 	}
 	
+	@Override
 	public Number add(Number number) {
 		if(number instanceof Int) {
 			return valueOf(value + ((Int)number).getValue());
@@ -30,6 +44,7 @@ public class Int implements SExpression, Number {
 		return lisp.eval.Double.valueOf(value + ((lisp.eval.Double)number).getValue());
 	}
 	
+	@Override
 	public Number sub(Number number) {
 		if(number instanceof Int) {
 			return valueOf(value - ((Int)number).getValue());
@@ -37,6 +52,7 @@ public class Int implements SExpression, Number {
 		return lisp.eval.Double.valueOf(value - ((lisp.eval.Double)number).getValue());
 	}
 	
+	@Override
 	public Number multiply(Number number) {
 		if(number instanceof Int) {
 			return valueOf(value * ((Int)number).getValue());
@@ -44,6 +60,7 @@ public class Int implements SExpression, Number {
 		return lisp.eval.Double.valueOf(value * ((lisp.eval.Double)number).getValue());
 	}
 	
+	@Override
 	public Number divide(Number number) throws LispException {
 		if(number instanceof Int) {
 			if(((Int)number).getValue() == 0) {
@@ -57,21 +74,25 @@ public class Int implements SExpression, Number {
 		return lisp.eval.Double.valueOf(value / ((lisp.eval.Double)number).getValue());
 	}
 	
+	@Override
 	public boolean lessThan(Number number) {
 		double comparedNumber = (number instanceof Int)?((Int)number).getValue():((lisp.eval.Double)number).getValue();
 		return (value < comparedNumber)?true:false;
 	}
 	
+	@Override
 	public boolean greaterThan(Number number) {
 		double comparedNumber = (number instanceof Int)?((Int)number).getValue():((lisp.eval.Double)number).getValue();
 		return (value > comparedNumber)?true:false;
 	}
 	
+	@Override
 	public boolean lessThanOrEqual(Number number) {
 		double comparedNumber = (number instanceof Int)?((Int)number).getValue():((lisp.eval.Double)number).getValue();
 		return (value <= comparedNumber)?true:false;
 	}
 	
+	@Override
 	public boolean greaterThanOrEqual(Number number) {
 		double comparedNumber = (number instanceof Int)?((Int)number).getValue():((lisp.eval.Double)number).getValue();
 		return (value >= comparedNumber)?true:false;
