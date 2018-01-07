9a206f565a3538506e519f30a5312af1b3fa8dbe
sam0830 <samcreate0830@gmail.com>
2018-01-08 00:58:49 +0900

    Doubleのドキュメンテーションコメントを追加

--- src/lisp/eval/Double.java
+++ src/lisp/eval/Double.java
@@ -4,26 +4,45 @@ import lisp.exception.ArgumentException;
 import lisp.exception.LispException;
 
 /**
- * 整数値
+ * 実数を表すS式
  * @author sam0830
- *
+ * @version 1.0
  */
 public class Double implements SExpression, Number {
 	private java.lang.Double value;
 	
+	/**
+	 * double型の値を返す
+	 * @return double型の変数
+	 */
 	public java.lang.Double getValue() {
 		return value;
 	}
-
+	
+	/**
+	 * 実数型のS式のインスタンスを返す
+	 * @param value 実数のS式の中身のdouble
+	 * @return 実数型のS式のインスタンス
+	 */
 	public static lisp.eval.Double valueOf(double value) {
 		return new lisp.eval.Double(value);
 	}
 	
+	/**
+	 * コンストラクタ
+	 * @param value double型の実数
+	 */
 	private Double(java.lang.Double value) {
 		this.value = value;
 	}
 	
-	// Double型での加減乗除はすべてDoubleで帰る
+	/**
+	 * 引数に与えられた数値との加算を行う
+	 * Int同士の場合はIntを返す
+	 * doubleが計算に含まれている場合はDoubleを返す
+	 * @param 加算する数値
+	 * @return 加算後の数値
+	 */
 	public Number add(Number number) {
 		if(number instanceof Int) {
 			return valueOf(value + ((Int)number).getValue());
@@ -31,6 +50,13 @@ public class Double implements SExpression, Number {
 		return valueOf(value + ((lisp.eval.Double)number).getValue());
 	}
 	
+	/**
+	 * 引数に与えられた数値との減算を行う
+	 * Int同士の場合はIntを返す
+	 * doubleが計算に含まれている場合はDoubleを返す
+	 * @param 減算する数値
+	 * @return 減算後の数値
+	 */
 	public Number sub(Number number) {
 		if(number instanceof Int) {
 			return valueOf(value - ((Int)number).getValue());
@@ -38,6 +64,13 @@ public class Double implements SExpression, Number {
 		return valueOf(value - ((lisp.eval.Double)number).getValue());
 	}
 	
+	/**
+	 * 引数に与えられた数値との乗算を行う
+	 * Int同士の場合はIntを返す
+	 * doubleが計算に含まれている場合はDoubleを返す
+	 * @param 乗算する数値
+	 * @return 乗算後の数値
+	 */
 	public Number multiply(Number number) {
 		if(number instanceof Int) {
 			return valueOf(value * ((Int)number).getValue());
@@ -45,8 +78,14 @@ public class Double implements SExpression, Number {
 		return valueOf(value * ((lisp.eval.Double)number).getValue());
 	}
 	
+	/**
+	 * 引数に与えられた数値との除算を行う
+	 * Int同士の場合はIntを返す
+	 * doubleが計算に含まれている場合はDoubleを返す
+	 * @param 除算する数値
+	 * @return 除算後の数値
+	 */
 	public Number divide(Number number) throws LispException {
-		
 		if(number instanceof Int) {
 			if(((Int)number).getValue() == 0) {
 				throw new ArgumentException("attempt to calculate a division by zero");
@@ -59,21 +98,49 @@ public class Double implements SExpression, Number {
 		return valueOf(value / ((lisp.eval.Double)number).getValue());
 	}
 	
+	/**
+	 * 引数に与えられた数値との大小比較を行う
+	 * 引数で与えられた数値のほうが小さい時true
+	 * 引数で与えられた数値のほうが小さくない時false
+	 * @param 比較対象の数値
+	 * @return 比較結果の真理値
+	 */
 	public boolean lessThan(Number number) {
 		double comparedNumber = (number instanceof Int)?((Int)number).getValue():((lisp.eval.Double)number).getValue();
 		return (value < comparedNumber)?true:false;
 	}
 	
+	/**
+	 * 引数に与えられた数値との大小比較を行う
+	 * 引数で与えられた数値のほうが大きい時true
+	 * 引数で与えられた数値のほうが大きくない時false
+	 * @param 比較対象の数値
+	 * @return 比較結果の真理値
+	 */
 	public boolean greaterThan(Number number) {
 		double comparedNumber = (number instanceof Int)?((Int)number).getValue():((lisp.eval.Double)number).getValue();
 		return (value > comparedNumber)?true:false;
 	}
 	
+	/**
+	 * 引数に与えられた数値との大小比較を行う
+	 * 引数で与えられた数値のほうが数値以下時true
+	 * 引数で与えられた数値のほうが数値以下でない時false
+	 * @param 比較対象の数値
+	 * @return 比較結果の真理値
+	 */
 	public boolean lessThanOrEqual(Number number) {
 		double comparedNumber = (number instanceof Int)?((Int)number).getValue():((lisp.eval.Double)number).getValue();
 		return (value <= comparedNumber)?true:false;
 	}
 	
+	/**
+	 * 引数に与えられた数値との大小比較を行う
+	 * 引数で与えられた数値のほうが数値以上時true
+	 * 引数で与えられた数値のほうが数値以上でない時false
+	 * @param 比較対象の数値
+	 * @return 比較結果の真理値
+	 */
 	public boolean greaterThanOrEqual(Number number) {
 		double comparedNumber = (number instanceof Int)?((Int)number).getValue():((lisp.eval.Double)number).getValue();
 		return (value >= comparedNumber)?true:false;
