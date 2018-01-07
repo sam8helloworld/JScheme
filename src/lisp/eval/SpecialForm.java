acac280b14267e4d97ed0e5359f39cc33e7d54aa
sam0830 <samcreate0830@gmail.com>
2018-01-08 03:20:55 +0900

    SpecialFormのドキュメンテーションコメントを追加

--- src/lisp/eval/SpecialForm.java
+++ src/lisp/eval/SpecialForm.java
@@ -3,9 +3,9 @@ package lisp.eval;
 import lisp.exception.LispException;
 
 /**
- * Special form (特殊形式)
+ * 特殊形式を表すインタフェース
  * @author sam0830
- *
+ * @version 1.0
  */
 interface SpecialForm extends SExpression {
 	public SExpression apply(SExpression sexp, Environment environment) throws LispException;
